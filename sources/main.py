fcc3a69de3f8c07399b224a6e6fe67fc535bed58
Emre Acarsoy <ea616@exeter.ac.uk>
2025-02-06 23:54:28 +0000

    working

--- sources/main.py
+++ sources/main.py
@@ -1,24 +1,32 @@
+import json
+
 import lift
-from request import Request
+from request import Request # not used currently
+from request_simulator import generate_random_requests
+
+
+CONFIG_FILENAME: str = "config.json"
+
+def read_config(filename: str = CONFIG_FILENAME) -> int:
+    with open('sources/config.json', 'r') as f:
+        data = json.load(f)
+        return (data["floors"])
 
 
 def main():
+
+    MAX_FLOOR: int = read_config() - 1 # we are starting from floor 0, hence the - 1
+
+    random_requests = generate_random_requests(n_requests=10, max_floor=MAX_FLOOR, seed=42)
+    
     Lift = lift.Lift()
 
-    request1 = Request(2, 5)
-    request2 = Request(1, 3)
-    request3 = Request(2, 3)
-    request4 = Request(4, 2)
-    request5 = Request(5, 1)
-
-    example_requests = [request1, request2, request3, request4, request5]
-
-    Lift.print()
-
-    for req in example_requests:
+    for req in random_requests:
         Lift.queue.add_request(req)
 
     Lift.print()
+
+
 
 if __name__ == "__main__":
     main()
--- sources/request_simulator.py
+++ sources/request_simulator.py
@@ -1,3 +1,17 @@
-class Simulator:
-    pass
+import random
 
+from request import Request
+
+def generate_random_requests(n_requests: int, max_floor: int, seed: int = None) -> list[Request]:
+    requests = []
+    for _ in range(n_requests):
+        # prevent start_floor == end_floor in generation:
+        start_floor = random.randint(1, max_floor)
+        end_floor = start_floor
+
+        while end_floor == start_floor:
+            end_floor = random.randint(1, max_floor)
+        
+        req = Request(start_floor, end_floor)
+        requests.append(req)
+    return requests
