b234284a1c2ff8ad335059b9a07914e14e6e3655
Emre Acarsoy <emreacarsoy09@gmail.com>
2025-02-25 22:24:53 +0000

    improvements to clarity and modularity of next_stop() method

--- sources/lift.py
+++ sources/lift.py
@@ -33,8 +33,18 @@ class Lift:
         self.onboard_requests = []       # Requests already picked up
         self.direction = Direction.NONE  # enum, UP, DOWN, or NONE
 
-    def add_request(self, req: Request) -> None:
-        self.request_queue.add_request(req)
+
+    def __filter_candidates(self, candidates: list[int], compare, select) -> int | None:
+        """
+        Helper function to filter candidate requests in next_stop() method.
+        Retrurns the selected floor, or None if no valid candidate.
+        Compare takes in an inline (lambda) function which compares the candidate to the current floor based on 
+        the lift's current direction (so x>y or x<y based on if lift direction is upward or downward).
+        Select takes in a function, min if upward, max if downward. 
+        """
+        valid_floors: list[int] = [floor for floor in candidates if compare(floor, self.current_floor) == True]
+        return select(valid_floors) if valid_floors else None
+    
     
     def next_stop(self) -> int | None:
         """
@@ -45,8 +55,8 @@ class Lift:
         The lift has a direction, up or down. If the lift is upbound, the next_stop() function filters requests by ones which 
         are upbound, and picks them up if their origin floor is the lift's current floor, and the lift is not full.
         """
-
         candidates = []
+
         # include onboard requests (their destination floors)
         for req in self.onboard_requests:
             candidates.append(req.destination_floor)
@@ -55,37 +65,35 @@ class Lift:
             for req in self.request_queue.get_requests():
                 candidates.append(req.origin_floor)
 
-        if not candidates:
-            return None
-
-        # if idle, pick a direction (we arbitrarily assign it to UP, if there are no upward requests the next_step() method 
-        # will take care of that anyway)
         if self.direction == Direction.NONE:
-            self.direction = Direction.UP
+            self.direction = Direction.UP # if the lift is idle, we arbitrarily set direction to up
+            # if there are no upward requests, the rest of this method will deal with it appropriately
 
         # filter the candidates based on the direction of the lift
         # Explanation: if the lift is moving upward, we do not want to pick up any requests which want to go downwards
         # so we filter those requests which are upward
         if self.direction == Direction.UP:
-            up_candidates = [floor for floor in candidates if floor > self.current_floor]
-            if up_candidates:
-                return min(up_candidates)
-            else:
-                # no one wants to go upwards (no valid up_candidates) so we switch direction
-                down_candidates = [floor for floor in candidates if floor < self.current_floor]
-                if down_candidates:
-                    self.direction = Direction.DOWN
-                    return max(down_candidates)
+            next_up = self.__filter_candidates(candidates, lambda x, y: x > y, min)
+            if next_up is not None:
+                return next_up
+            # no one wants to go upwards (no valid up_candidates) so we switch direction
+            next_down = self.__filter_candidates(candidates, lambda x, y: x < y, max)
+            if next_down is not None:
+                self.direction = Direction.DOWN  # we switched direction, so we update self.direction
+                return next_down
+        
         elif self.direction == Direction.DOWN:
-            down_candidates = [floor for floor in candidates if floor < self.current_floor]
-            if down_candidates:
-                return max(down_candidates)
-            else:
-                up_candidates = [floor for floor in candidates if floor > self.current_floor]
-                if up_candidates:
-                    self.direction = Direction.UP
-                    return min(up_candidates)
-        return None
+            next_down = self.__filter_candidates(candidates, lambda x, y: x < y, max)
+            if next_down is not None:
+                return next_down
+            # if no downward candidates were found, we switch direction to upwards
+            next_up = self.__filter_candidates(candidates, lambda x, y: x > y, min)
+            if next_up is not None:
+                self.direction = Direction.UP  # we switched direction, so we update self.direction
+                return next_up
+            
+        return None  # if no candidates were valid, we return None and the lift goes idle
+
 
     def move(self) -> None:
         next_stop = self.next_stop()
@@ -96,18 +104,13 @@ class Lift:
         # make a move -> up / down 1 floor
         if self.current_floor < next_stop:
             self.current_floor += 1
-            self.direction = Direction.UP      # make sure direction of the lift is updated
         elif self.current_floor > next_stop:
             self.current_floor -= 1
-            self.direction = Direction.DOWN    # make sure direction of the lift is updated
-        
-        # print(f"Moving {self.direction} to floor {self.current_floor}")
 
         # drop off any onboard requests that have reached their destination.
         served_requests = [req for req in self.onboard_requests if req.destination_floor == self.current_floor]
         for req in served_requests:
             self.onboard_requests.remove(req)
-            # print(f"Served: {req}")
 
         # IF the lift is at the origin of a request, AND the capacity is not full, we pick up the request
         # we iterate over a COPY of the queue (.copy()) since we might modify it
@@ -117,11 +120,11 @@ class Lift:
                 req.picked_up = True
                 self.onboard_requests.append(req)
                 self.request_queue.remove_request(req)
-                # print(f"Picked up: {req}")
 
         # # if there are no more requests, and no one onboard, reset direction to None
         if not self.request_queue.get_requests() and not self.onboard_requests:
             self.direction = Direction.NONE
+
 
     def run(self) -> None:
         """Run the lift simulation until all requests are served."""
@@ -129,6 +132,7 @@ class Lift:
             self.move()
             # print(self)
             time.sleep(0.5)
+
 
     def __repr__(self) -> str:
         return (f"Lift(current_floor={self.current_floor}, "
