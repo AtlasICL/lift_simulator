5d6e0c46d949a12dcb11732ec94d41c416f238b9
Emre Acarsoy <emreacarsoy09@gmail.com>
2025-02-25 14:16:53 +0000

    removed __is_valid_request() functionality from Request class, as it does not need to know. The CALLER of the request is responsible for verifying the request is 0 < req < number_floors

--- sources/request.py
+++ sources/request.py
@@ -4,24 +4,10 @@ class Request:
     In our simulation, a request indicates a call to the lift from an origin floor to a destination floor.
     """
 
-    def __init__(self, origin: int, destination: int, total_floors: int):
-        self.NUM_FLOORS = total_floors
-        if not self.is_valid_request(origin, destination):
-            raise ValueError(
-                f"Invalid request: {origin} -> {destination} (must be within 1 and {total_floors} and not equal)"
-            )
+    def __init__(self, origin: int, destination: int):
         self.origin_floor = origin
         self.destination_floor = destination
         self.picked_up = False 
-
-    def is_valid_request(self, origin_floor: int, destination_floor: int) -> bool:
-        if origin_floor < 1 or destination_floor < 1:
-            return False
-        if origin_floor > self.NUM_FLOORS or destination_floor > self.NUM_FLOORS:
-            return False
-        if origin_floor == destination_floor:
-            return False
-        return True
 
     def is_upward(self) -> bool:
         return self.destination_floor > self.origin_floor
@@ -30,5 +16,8 @@ class Request:
         return self.destination_floor < self.origin_floor
 
     def __repr__(self) -> str:
+        """
+        function for printing to the console
+        """
         status = "picked up" if self.picked_up else "waiting"
         return f"Request({self.origin_floor} -> {self.destination_floor}, {status})"
